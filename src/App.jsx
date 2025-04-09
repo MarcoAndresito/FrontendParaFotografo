655e4f9b50acaa2717aa1222925fad6b2bb3085b
Neycer-Nay <neyser.nay@gmail.com>
2025-04-08 21:20:38 -0400

    Mostrando en el layout del proyectos los inpust creados de registro de usuario

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import reactLogo from "./assets/react.svg";
 import About from "./pages/About";
 import Home from "./pages/Home";
+import RegisterUser from "./pages/RegisterUser/RegisterUser";
 import viteLogo from "/vite.svg";
 
 const App = () => {
@@ -17,10 +18,13 @@ const App = () => {
         </a>
         <Link to="/">Inicio</Link>
         <Link to="/about">Acerca de</Link>
+        <Link to="/RegisterUser">Registro de usuario</Link>
+        
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/RegisterUser" element= {<RegisterUser/>}/>
       </Routes>
     </BrowserRouter>
   );
--- src/pages/RegisterUser/RegisterUser.jsx
+++ src/pages/RegisterUser/RegisterUser.jsx
@@ -12,3 +12,5 @@ const RegisterUser = () => {
         </div>
     );
 };
+
+export default RegisterUser; 
