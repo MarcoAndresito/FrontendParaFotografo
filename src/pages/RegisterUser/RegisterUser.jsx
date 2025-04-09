72918d25a5e366bc8deaf1caf2f772452fbf5bb2
Neycer-Nay <neyser.nay@gmail.com>
2025-04-08 21:06:12 -0400

    Creando carpeta RegisterUser y componente con sus inputs

---
+++ src/pages/RegisterUser/RegisterUser.jsx
@@ -0,0 +1,14 @@
+const RegisterUser = () => {
+    return (
+        <div>
+            <form action="">
+                <h2>Registro de Usuario</h2>
+                <input type="text" placeholder="Escriba su nombre" />
+                <input type="email" name="" placeholder="Escriba su correo" id="" />
+                <input type="password" name="" placeholder="Escriba su contraseña" id="" />
+                <input type="password" name="" placeholder="Confirme su contraseña" id="" />
+                <button type="submit">Registrarse</button>
+            </form>
+        </div>
+    );
+};
