d89691a4dd34a815e82b7610e2fc3b7f7d2e64b6
Neycer-Nay <neyser.nay@gmail.com>
2025-04-14 20:12:56 -0400

    Mejorando el diseño final del registro usuario

--- src/pages/RegisterUser/RegisterUser.jsx
+++ src/pages/RegisterUser/RegisterUser.jsx
@@ -1,16 +1,29 @@
 import styles from "./RegisterUser.module.css";
 
+
 const RegisterUser = () => {
     return (
         <div className={styles.container}>
             <form action="" className={styles.form}>
-                <h2 className={styles.titulo}>Registro de Usuario</h2>
+                <h2  className={styles.titulo}>REGISTRO DE USUARIO</h2>
+                <label className={styles.label} htmlFor="">Nombre</label>
+
                 <input className={styles.input} type="text" placeholder="Escriba su nombre" />
+
+                <label className={styles.label} htmlFor="">Correo</label>
+
                 <input className={styles.input} type="email" name="" placeholder="Escriba su correo" id="" />
+
+                <label className={styles.label} htmlFor="">Contraseña</label>
+
                 <input className={styles.input} type="password" name="" placeholder="Escriba su contraseña" id="" />
+
+                <label className={styles.label} htmlFor="">Repita su contraseña</label>
+
                 <input className={styles.input} type="password" name="" placeholder="Confirme su contraseña" id="" />
+
                 <button className={styles.buton} type="submit">Registrarse</button>
-                <button className={styles.buton} type="submit">Olvidaste tu Contraseña</button>
+                
             </form>
         </div>
     );
