f0259c54c6f3427fb156500b66899706aeed9aa8
Aguil3ra <mariajoseaguileraaguirre71@gmail.com>
2025-04-16 15:45:11 -0400

    creacion de la rama, y los archivos para el css y jsx, ademas de la implementacion de las rutas en el app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import UploadPhoto from "./pages/UploadPhoto";  // Importar UploadPhoto desde pa
 import RegisterUser from "./pages/RegisterUser/RegisterUser";
 import viteLogo from "/vite.svg";
 import Autenticacion from "./pages/Autenticacionloginsegura/Autenticacion";
+import EdicionPerfil from "./pages/EdicionDePerfil/EditarPerfil";
 
 const App = () => {
   return (
@@ -27,6 +28,7 @@ const App = () => {
         <Link to="/RegisterUser">Registro de usuario</Link>
         
         <Link to="/Autenticacion">Autenticación Segura</Link>
+        <Link to="/EdicionPerfil">Editar Perfil</Link> {/* Enlace a la página de edición de perfil */}
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
@@ -35,6 +37,7 @@ const App = () => {
         <Route path="/uploadphoto" element={<UploadPhoto />} /> {/* Ruta correcta para UploadPhoto */}
         <Route path="/RegisterUser" element= {<RegisterUser/>}/>
         <Route path="/Autenticacion" element={<Autenticacion />} />
+        <Route path="/EdicionPerfil" element={<EdicionPerfil />} /> {/* Ruta correcta para EdicionPerfil */}
       </Routes>
     </BrowserRouter>
   );
