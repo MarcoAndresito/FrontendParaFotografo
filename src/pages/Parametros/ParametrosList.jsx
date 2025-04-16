0b5662b5781ec834c81d81c4a47e54dcd9340ff7
Eithan21 <109700765+Eithan21@users.noreply.github.com>
2025-04-16 15:20:11 -0400

    Crear botones y estilos para las opciones de ver, editar y eliminar en listar parametros

--- src/pages/Parametros/ParametrosList.jsx
+++ src/pages/Parametros/ParametrosList.jsx
@@ -1,32 +1,71 @@
 import React from 'react';
 import styles from './Parametros.module.css';
+import { useEffect } from "react";
+import { useState } from "react";
 
   const ParametrosList = () => {
-    
+
+    const [parametros, setParametros] = useState([]);
+    const obtenerProductos = () => {
+      fetch("http://localhost:5188/api/Parametros", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Respuesta del backend:", data);
+          setParametros(data);
+        })
+        .catch((error) => {
+          console.log("error:", error);
+        });
+    };
+  
+    useEffect(() => {
+      obtenerProductos();
+    }, []);
+
     return (
       <div className={styles.contenedor}>
         <h2 className={styles.titulo}>Lista de Parámetros</h2>
         <table className={styles.tabla}>
-          <thead>
+            <thead>
             <tr>
-              <th>ID</th>
-              <th>Servicio</th>
-              <th>Clave</th>
-              <th>Valor</th>
-              <th>Descripción</th>
-              <th>Activo</th>
+                <th>ID</th>
+                <th>Servicio</th>
+                <th>Clave</th>
+                <th>Valor</th>
+                <th>Descripción</th>
+                <th>Acciones</th>
             </tr>
-          </thead>
-          <tbody>
-                <tr >
-                  <td>ss</td>
-                  <td>ss</td>
-                  <td>ss</td>
-                  <td>sdasdsa</td>
-                  <td>dasd</td>
-                  <td>asds</td>
+            </thead>
+            <tbody>
+            {parametros.length > 0 ? (
+                parametros.map((parametro) => (
+                <tr key={parametro.id}>
+                    <td>{parametro.id}</td>
+                    <td>{parametro.servicio}</td>
+                    <td>{parametro.clave}</td>
+                    <td>{parametro.valor}</td>
+                    <td>{parametro.descripcion}</td>
+                    <td className={styles.acciones}>
+                        <button className={`${styles.boton} ${styles.ver}`} >Ver</button>
+                        <button className={`${styles.boton} ${styles.editar}`} >Editar</button>
+                        <button className={`${styles.boton} ${styles.eliminar}`}>Eliminar</button>
+                    </td>
                 </tr>
-          </tbody>
+                ))
+            ) : (
+                <tr>
+                    <td colSpan="3"></td>
+                    <td colSpan="3" className={styles.sinDatos}>
+                        No hay parámetros registrados.
+                    </td>
+                </tr>
+            )}
+            </tbody>
         </table>
       </div>
     );
