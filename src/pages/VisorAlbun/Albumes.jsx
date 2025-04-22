c4b16bcedcc3ecc39cf62ce1b62113b525365255
Neycer-Nay <neyser.nay@gmail.com>
2025-04-22 19:54:41 -0400

    Definimos nuestro componente albumes con una funcion flecha y usamos los estados, usamos useEffet para cargar los albumes

--- src/pages/VisorAlbun/Albumes.jsx
+++ src/pages/VisorAlbun/Albumes.jsx
@@ -2,3 +2,22 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Albumes.module.css"; 
 
+const Albumes = () => {
+    const [albums, setAlbums] = useState([]);
+    const [loading, setLoading] = useState(true);
+  
+  
+    useEffect(() => {
+      fetch("http://localhost:3000/albumes")
+        .then((res) => res.json())
+        .then((data) => {
+          setAlbums(data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.error("Error al cargar los álbumes:", error);
+          setLoading(false);
+        });
+    }, []);
+  
+    if (loading) return <p>Cargando álbumes...</p>;
