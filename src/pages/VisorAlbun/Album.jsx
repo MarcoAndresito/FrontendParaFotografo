45366ed736709de4ab79a93ed5e953dffdd98d5e
Neycer-Nay <neyser.nay@gmail.com>
2025-04-22 20:43:28 -0400

    Imprtamos los hooks, estilos y la libreria de slick carrusel

---
+++ src/pages/VisorAlbun/Album.jsx
@@ -0,0 +1,8 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import styles from "./Album.module.css";
+import React from "react";
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
