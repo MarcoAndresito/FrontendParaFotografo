330de688c1259c232a5d795f58e4e96dc4ee86a2
icv-9 <154081131+icv-9@users.noreply.github.com>
2025-04-26 17:17:56 -0400

    feat: Agregando color de fondo diferente en el hover de cada menú y mostrar al pasar por encima

--- src/App.jsx
+++ src/App.jsx
@@ -25,52 +25,51 @@ import ModComentarios from "./pages/Comentarios/ModComentarios";
 import Moderador from "./pages/Comentarios/Moderador";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <nav>
-        <a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <Link to="/">Inicio</Link>
-        <Link to="/about">Acerca de</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/uploadphoto">Álbum</Link>{" "}
-        {/* Enlace a la página de carga de fotos */}
-        <Link to="/RegisterUser">Registro de usuario</Link>
-        <Link to="/Autenticacion">Autenticación Segura</Link>
-        <Link to="/ExportarAlbum">Exportar Album</Link> {/* Enlace a la página de carga de fotos */}
-        <Link to="/comentarios">Comentarios</Link>
-        <Link to="/Productos">Productos</Link>
-        <Link to="/Parametros">Parametros</Link>
-        <Link to="/ConfigurarAlbum">Configurar Álbum</Link>
-        <Link to="/Comentarios">Comentarios</Link>
-      </nav>
+    return (
+        <BrowserRouter>
+            <nav>
+                <a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                    </a>
+                <Link to="/" className="menu-inicio">Inicio</Link>
+                <Link to="/about" className="menu-acerca">Acerca de</Link>
+                <Link to="/login" className="menu-login">Login</Link>
+                <Link to="/uploadphoto" className="menu-album">Álbum</Link>
+                <Link to="/RegisterUser" className="menu-registro">Registro de usuario</Link>
+                <Link to="/Autenticacion" className="menu-autenticacion">Autenticación Segura</Link>
+                <Link to="/ExportarAlbum" className="menu-exportar">Exportar Album</Link>
+                <Link to="/comentarios" className="menu-comentarios">Comentarios</Link>
+                <Link to="/Productos" className="menu-productos">Productos</Link>
+                <Link to="/Parametros" className="menu-parametros">Parametros</Link>
+                <Link to="/ConfigurarAlbum" className="menu-configurar-album">Configurar Álbum</Link>
+                <Link to="/Comentarios" className="menu-comentarios-duplicado">Comentarios</Link>
+            </nav>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="login" element={<Login />} />
-        <Route path="/uploadphoto" element={<UploadPhoto />} />{" "}
-        {/* Ruta correcta para UploadPhoto */}
-        <Route path="/RegisterUser" element={<RegisterUser />} />
-        <Route path="/Autenticacion" element={<Autenticacion />} />
-        <Route path="/ExportarAlbum" element={<ExportarAlbum />} /> {/* Ruta correcta para ExportarAlbum */}
-        <Route path="/comentarios" element={<Coment />} />
-        <Route path="/Productos" element={<ProductosList />} />
-        <Route path="/Parametros" element={<ParametrosList />} />
-        <Route path="/Productos/:id" element={<ProductosUpd />} />
-        <Route path="/Productos/new" element={<ProductosAdd />} />
-        <Route path="/Parametros/new" element={<ParametrosAdd />} />
-        <Route path="/ConfigurarAlbum" element={<ConfigurarAlbum />} />
-        <Route path="/Comentarios" element={<Comentarios />} />
-        <Route path="/ModComentarios" element={<ModComentarios />} />
-        <Route path="/Moderador" element={<Moderador />} />
-      </Routes>
-    </BrowserRouter>
-  );
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="login" element={<Login />} />
+                <Route path="/uploadphoto" element={<UploadPhoto />} />{" "}
+                {/* Ruta correcta para UploadPhoto */}
+                <Route path="/RegisterUser" element={<RegisterUser />} />
+                <Route path="/Autenticacion" element={<Autenticacion />} />
+                <Route path="/ExportarAlbum" element={<ExportarAlbum />} /> {/* Ruta correcta para ExportarAlbum */}
+                <Route path="/comentarios" element={<Coment />} />
+                <Route path="/Productos" element={<ProductosList />} />
+                <Route path="/Parametros" element={<ParametrosList />} />
+                <Route path="/Productos/:id" element={<ProductosUpd />} />
+                <Route path="/Productos/new" element={<ProductosAdd />} />
+                <Route path="/Parametros/new" element={<ParametrosAdd />} />
+                <Route path="/ConfigurarAlbum" element={<ConfigurarAlbum />} />
+                <Route path="/Comentarios" element={<Comentarios />} />
+                <Route path="/ModComentarios" element={<ModComentarios />} />
+                <Route path="/Moderador" element={<Moderador />} />
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default App;
