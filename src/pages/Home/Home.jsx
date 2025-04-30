24168a2ba53e6c9b7b62bbe1aa11bddd4c93c777
Eithan21 <109700765+Eithan21@users.noreply.github.com>
2025-04-30 18:51:57 -0400

    Cambiar nombre del archivo Home.css por Home.Module.css

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import "./Home.css"; // <-- Asegúrate de importar el CSS aquí
+import "./Home.modeule.css"; // <-- Asegúrate de importar el CSS aquí
 
 const Home = () => {
     const navigate = useNavigate();
