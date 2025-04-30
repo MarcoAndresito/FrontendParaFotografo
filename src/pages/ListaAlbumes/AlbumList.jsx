2345659b8c7b55161dfa662c3743d8765f62446c
Aguil3ra <mariajoseaguileraaguirre71@gmail.com>
2025-04-29 22:34:51 -0400

    agregacion de los botones

--- src/pages/ListaAlbumes/AlbumList.jsx
+++ src/pages/ListaAlbumes/AlbumList.jsx
@@ -6,7 +6,7 @@ const AlbumList = () => {
     const [albumes, setAlbumes] = useState([]);
 
     const obtenerAlbumes = () => {
-        fetch("https://localhost:7062/api/Albumes", {
+        fetch("https://localhost:44302/api/Albumes", {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
@@ -22,32 +22,39 @@ const AlbumList = () => {
                 console.log("error:", error);
             });
     };
-
+useEffect(() => {
+    obtenerAlbumes(); 
+}, []);
     return (
-        <div className={styles.container}> 
-          <h1 className={styles.title}>------Lista de Álbumes------</h1> 
-          <table className={styles.table}> 
-            <thead className={styles.thead}> 
-              <tr>
-                <th className={styles.th}> ID </th> 
-                <th className={styles.th}> Nombre </th>
-                <th className={styles.th}> Descripción </th>
-                <th className={styles.th}> Acciones </th>
+      <div className={styles.container}>
+        <h1 className={styles.title}>------Lista de Álbumes------</h1>
+        <button className={styles.addButton}>Agregar nuevo álbum</button>  
+        <table className={styles.table}>
+          <thead className={styles.thead}>
+            <tr>
+              <th className={styles.th}> ID </th>
+              <th className={styles.th}> Nombre </th>
+              <th className={styles.th}> Descripción </th>
+              <th className={styles.th}> Acciones </th>
+            </tr>
+          </thead>
+          <tbody className={styles.tbody}>
+            {albumes.map((album) => (
+              <tr key={album.id}>
+                <td className={styles.td}>{album.id}</td>
+                <td className={styles.td}>{album.nombre}</td>
+                <td className={styles.td}>{album.descripcion}</td>
+                <td className={styles.td}> 
+                  <button className={styles.actionsButton}>Subir foto</button>
+                  <button className={styles.actionsButton}>Descargar álbum</button>
+                  <button className={styles.actionsButton}>Editar</button>
+                  <button className={styles.actionsButton}>Eliminar</button>
+                </td> 
               </tr>
-            </thead>
-            <tbody className={styles.tbody}>
-              {albumes.map((album) => (
-                <tr key={album.id}>
-                  <td className={styles.td}>{album.id}</td> 
-                  <td className={styles.td}>{album.nombre}</td>
-                  <td className={styles.td}>{album.descripcion}</td>
-                  <td className={styles.td}>{album.acciones}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      );
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
 }
-
 export default AlbumList;
